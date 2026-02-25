87b8b9ed326ab6079c6a479c1eb02b74fc50311e
klvelasquezc <47877185+klvelasquezc@users.noreply.github.com>
2026-02-25 13:06:21 -0600

    Delete piecewiseSEM-main directory

--- piecewiseSEM-main/R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-
-  packageStartupMessage("
-  This is piecewiseSEM version 2.3.01.\n
-
-  Questions or bugs can be addressed to <jlefcheck@umces.edu>.")
-  
-  }
